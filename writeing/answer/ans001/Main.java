c87e9b0ce70de2bf1cc3687a5ff8095226bae7aa
herco-mitex <htkr1126@icloud.com>
2024-11-26 09:06:34 +0900

    Create Main.java

---
+++ writeing/answer/ans001/Main.java
@@ -0,0 +1,13 @@
+package JavaExercises.writeing.answer.ans001;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 5;
+        java.lang.System.out.println("　和: " + (n + m));
+        java.lang.System.out.println("　差: " + (n - m));
+        java.lang.System.out.println("　積: " + (n * m));
+        java.lang.System.out.println("　商: " + (n / m));
+        java.lang.System.out.println("余り: " + (n % m));
+    }
+}
