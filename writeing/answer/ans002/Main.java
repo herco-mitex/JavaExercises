be8851ac8bd0ed8e495701efeaa926c834ec8692
herco-mitex <htkr1126@icloud.com>
2024-11-26 09:11:42 +0900

    Create Main.java

---
+++ writeing/answer/ans002/Main.java
@@ -0,0 +1,10 @@
+package javaExercises.writing.answer.ans002;
+
+public class Main {
+    public static void main(String[] args) {
+        double n = 3;
+        double m = 5;
+        m = n;
+        java.lang.System.out.printf("n: %d\nm: %d\n", n, m);
+    }
+}
